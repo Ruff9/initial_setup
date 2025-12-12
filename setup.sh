17389fcb3e37f37959bf9528b46d0686ba89a0cc
Rémy Maucourt <remy.maucourt@yahoo.fr>
2025-12-12 19:47:31 +0100

    update script

--- setup.sh
+++ setup.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
 sudo apt update
+sudo apt upgrade
 sudo apt install curl wget git libfuse2t64 zsh
+
+# vanilla gnome
+
+sudo apt install pulseaudio
+sudo apt install vanilla-gnome-desktop
 
 # ohmyzsh
 chsh -s $(which zsh)
@@ -11,6 +17,10 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo tee /etc/apt/keyrings/sublimehq-pub.asc > /dev/null
 echo -e 'Types: deb\nURIs: https://download.sublimetext.com/\nSuites: apt/stable/\nSigned-By: /etc/apt/keyrings/sublimehq-pub.asc' | sudo tee /etc/apt/sources.list.d/sublime-text.sources
 
-sudo apt install sublime-text
+sudo apt-get update
+sudo apt-get install sublime-text
 
-sudo apt install terminator focuswriter audacious
+sudo apt install terminator audacious
+
+# reboot after script
+# select Gnome in the login screen
